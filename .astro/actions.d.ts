259bf3c76a0b98e829aa9ab268584bfcdd5fd38e
Robinson Rodriguez <robinson.rodriguez.aponte.-nd@disney.com>
2025-10-20 17:40:08 -0500

    Update ci setup

--- .astro/actions.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "astro:actions" {
-	type Actions = typeof import("C:/Users/robinson.rodriguez/projects/Astro/07-store/src/actions/index.ts")["server"];
-
-	export const actions: Actions;
-}
